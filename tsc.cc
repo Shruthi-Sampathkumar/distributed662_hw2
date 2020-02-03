4e726a8518386383d1e1eef56c920e91dba795a4
shruthi <shruthi@client-10-229-136-29.tamulink.tamu.edu>
2020-02-03 14:16:23 -0600

    development version1

---
+++ tsc.cc
@@ -0,0 +1,281 @@
+#include <iostream>
+#include <memory>
+//#include <thread>
+//#include <vector>
+#include <string>
+#include <unistd.h>
+#include <grpc++/grpc++.h>
+#include "client.h"
+
+#include "timeline.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientReaderWriter;
+using grpc::ClientWriter;
+using grpc::Status;
+
+using timeline::social_network;
+using timeline::user;
+using timeline::post;
+using timeline::follow_request;
+using timeline::follow_response;
+using timeline::unfollow_request;
+using timeline::unfollow_response;
+using timeline::list_request;
+using timeline::list_response;
+
+class Client : public IClient
+{
+    public:
+        Client(const std::string& hname,
+               const std::string& uname,
+               const std::string& p)
+            :hostname(hname), username(uname), port(p)
+            {}
+    //create a stub for the service social_network
+    
+    
+    Client(std::shared_ptr<Channel> channel, const std::string& db)
+        : stub_(social_network::NewStub(channel)) {}
+    
+
+    protected:
+        virtual int connectTo();
+        virtual IReply processCommand(std::string& input);
+        virtual void processTimeline();
+    private:
+        std::string hostname;
+        std::string username;
+        std::string port;
+        
+        // You can have an instance of the client stub
+        // as a member variable.
+        //std::unique_ptr<NameOfYourStubClass::Stub> stub_;
+        std::unique_ptr<social_network::Stub> stub_;
+    
+        std::string db = social_network::GetDbFileContent(argc, argv);
+};
+
+int main(int argc, char** argv) {
+
+    std::string hostname = "localhost";
+    std::string username = "default";
+    std::string port = "3010";
+    int opt = 0;
+    while ((opt = getopt(argc, argv, "h:u:p:")) != -1){
+        switch(opt) {
+            case 'h':
+                hostname = optarg;break;
+            case 'u':
+                username = optarg;break;
+            case 'p':
+                port = optarg;break;
+            default:
+                std::cerr << "Invalid Command Line Argument\n";
+        }
+    }
+
+    Client myc(hostname, username, port);
+    // You MUST invoke "run_client" function to start business logic
+    //this run() function in the interface runs the logic
+    myc.run_client();
+
+    return 0;
+}
+
+int Client::connectTo()
+{
+	// ------------------------------------------------------------
+    // In this function, you are supposed to create a stub so that
+    // you call service methods in the processCommand/porcessTimeline
+    // functions. That is, the stub should be accessible when you want
+    // to call any service methods in those functions.
+    // I recommend you to have the stub as
+    // a member variable in your own Client class.
+    // Please refer to gRpc tutorial how to create a stub.
+	// ------------------------------------------------------------
+    Client ci(
+        grpc::CreateChannel("localhost:50051",
+                            grpc::InsecureChannelCredentials()));
+    return 1; // return 1 if success, otherwise return -1
+}
+
+IReply Client::processCommand(std::string& input)
+{
+	// ------------------------------------------------------------
+	// GUIDE 1:
+	// In this function, you are supposed to parse the given input
+    // command and create your own message so that you call an 
+    // appropriate service method. The input command will be one
+    // of the followings:
+	//
+	// FOLLOW <username>
+	// UNFOLLOW <username>
+	// LIST
+    // TIMELINE
+	//
+	// - JOIN/LEAVE and "<username>" are separated by one space.
+	// ------------------------------------------------------------
+    
+    //parsing the input. 'newString1' will contain the parsed result
+    int j=0, ctr=0;
+    char newString1 [3][20];
+    for(int i=0;i<=(strlen(input));i++)
+    {
+        // if space or NULL found, assign NULL into newString[ctr]
+        if(input[i]==' '||input[i]=='\0')
+        {
+            newString1[ctr][j]='\0';
+            ctr++;  //for next word
+            j=0;    //for next word, init index to 0
+        }
+        else
+        {
+            newString1[ctr][j]=input[i];
+            j++;
+        }
+    }
+    
+    IReply ire;
+    command = newString1[0];
+    
+    if (strcmp(command "FOLLOW")==0)
+    {
+        std::vector<std::string> u1 = username;
+        std::vector<std::string> u2 = newString1[1];
+        //u1 = username;
+        //u2 = newString1[1];
+        
+        follow_request f1_request;
+        f1_request.set_allocated_user1(u1);
+        f1_request.set_allocated_user2(u2);
+        
+        ClientContext context;
+        follow_response f1_response;
+        
+        Status status = stub_->addTo(&context, f1_request, f1_response);
+        ire.grpc_status = status;
+        if (!status.ok())
+        {
+            ire.comm_status = FAILURE_INVALID;
+            std::cout << "addTo rpc failed." << std::endl;
+            return false;
+        }
+        else
+        {
+            ire.comm_status = SUCCESS;
+            std::cout << "Follow request successful : " << f1_response->success_status()  << std::endl;
+        }
+        
+        }
+    else if (strcmp(command, "UNFOLLOW")==0)
+    {
+        std::vector<std::string> u1 = username;
+        std::vector<std::string> u2 = newString1[1];
+        //u1 = username;
+        //u2 = newString1[1];
+        
+        unfollow_request f2_request;
+        f2_request.set_allocated_user1(u1);
+        f2_request.set_allocated_user2(u2);
+        
+        ClientContext context;
+        unfollow_response f2_response;
+        
+        Status status = stub_->removeFrom(&context, f2_request, f2_response);
+        ire.grpc_status = status;
+        if (!status.ok())
+        {
+            ire.comm_status = FAILURE_INVALID;
+            std::cout << "removeFrom rpc failed : " << std::endl;
+            return false;
+        }
+        else
+        {
+            ire.comm_status = SUCCESS;
+            std::cout << "Unfollow request successful : " << f2_response->success_status() << std::endl;
+        }
+    }
+    
+    else if (strcmp(command, "LIST")==0)
+    {
+        std::vector<std::string> u1 = username;
+        //u1 = username;
+        
+        list_request l_request;
+        l_request.set_allocated_user1(u1);
+        
+        ClientContext context;
+        list_response l_response;
+        
+        Status status = stub_->getFollowersUsers(&context, l_request, l_response);
+        ire.grpc_status = status;
+        if (!status.ok() or l_response.success_status()!=0)
+        {
+            ire.comm_status = FAILURE_INVALID;
+            std::cout << "getFollowersUsers rpc failed." << std::endl;
+            return false;
+        }
+        else
+        {
+            ire.comm_status = SUCCESS;
+            ire.following_users = l_response.followers();
+            ire.all_users = l_response.active_users();
+            std::cout << "List request successful " << std::endl;
+        }
+    }
+    // ------------------------------------------------------------
+	// GUIDE 2:
+	// Then, you should create a variable of IReply structure
+	// provided by the client.h and initialize it according to
+	// the result. Finally you can finish this function by returning
+    // the IReply.
+	// ------------------------------------------------------------
+    
+	// ------------------------------------------------------------
+    // HINT: How to set the IReply?
+    // Suppose you have "Join" service method for JOIN command,
+    // IReply can be set as follow:
+    // 
+    //     // some codes for creating/initializing parameters for
+    //     // service method
+    //     IReply ire;
+    //     grpc::Status status = stub_->Join(&context, /* some parameters */);
+    //     ire.grpc_status = status;
+    //     if (status.ok()) {
+    //         ire.comm_status = SUCCESS;
+    //     } else {
+    //         ire.comm_status = FAILURE_NOT_EXISTS;
+    //     }
+    //      
+    //      return ire;
+    // 
+    // IMPORTANT: 
+    // For the command "LIST", you should set both "all_users" and 
+    // "following_users" member variable of IReply.
+	// ------------------------------------------------------------
+    return ire;
+}
+
+void Client::processTimeline()
+{
+	// ------------------------------------------------------------
+    // In this function, you are supposed to get into timeline mode.
+    // You may need to call a service method to communicate with
+    // the server. Use getPostMessage/displayPostMessage functions
+    // for both getting and displaying messages in timeline mode.
+    // You should use them as you did in hw1.
+	// ------------------------------------------------------------
+
+    // ------------------------------------------------------------
+    // IMPORTANT NOTICE:
+    //
+    // Once a user enter to timeline mode , there is no way
+    // to command mode. You don't have to worry about this situation,
+    // and you can terminate the client program by pressing
+    // CTRL-C (SIGINT)
+	// ------------------------------------------------------------
+    
+}
---
+++ tsd.cpp
@@ -0,0 +1,200 @@
+//
+//  tsd.cpp
+//  
+//
+//  Created by Shruthi Sampath Kumar on 2/2/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include <bits/stdc++.h>
+
+#include <json/value.h>
+#include <fstream>
+
+#include "timeline.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using timeline::social_network;
+using timeline::user;
+using timeline::post;
+using timeline::follow_request;
+using timeline::follow_response;
+using timeline::unfollow_request;
+using timeline::unfollow_response;
+using timeline::list_request;
+using timeline::list_response;
+using timeline::addTo;
+using timeline::removeFrom;
+using timeline::getFollowersUsers;
+
+
+class timelineImpl final : public social_network::Service {
+public:
+ //explicit timelineImpl(const std::string& db) {
+   //timeline::ParseDb(db, &feature_list_);
+//}
+    
+    //if the command was "FOLLOW"
+    Status addTo(ServerContext* context, const follow_request* request,
+                    follow_response* response)
+    {
+        //reading the json file
+        std::ifstream ip_users_file("users.json", std::ifstream::binary);
+        if (!ip_users_file.is_open())
+        {
+          std::cout << "Failed to open users.json " << std::endl;
+          return ;
+        }
+        
+        ip_users_file >> users;
+        std::vector<std::string> u1 = request.user1().name();
+        std::vector<std::string> u2 = request.user2().name();
+        //user u1 = request.user1();
+        //user u2 = request.user2();
+        //std::vector<std::string> current_followers = users[u1]["followers"];
+        //current_followers.push_back(u2);
+        if (users.contains(u1) and users.contains(u2))
+        {
+            users["users"][u1]["following"].push_back(u2);
+            users["users"][u2]["followers"].push_back(u1);
+            std::ofstream op_users_file("users.json");
+            op_users_file << std::setw(4) << users << std::endl;
+            response.set_success_status(0);
+        }
+        else:
+        {
+            response.set_success_status(1);
+        }
+        
+      return Status::OK;
+    }
+    
+    //if the command was "UNFOLLOW"
+    Status removeFrom(ServerContext* context, const unfollow_request* request,
+    unfollow_response* response)
+    {
+        //reading the json file
+        std::ifstream ip_users_file("users.json", std::ifstream::binary);
+        if (!ip_users_file.is_open())
+        {
+          std::cout << "Failed to open users.json " << std::endl;
+          return ;
+        }
+        
+        ip_users_file >> users;
+        std::vector<std::string> u1 = request.user1().name();
+        std::vector<std::string> u2 = request.user2().name();
+        //user u1 = request.user1();
+        //user u2 = request.user2();
+        
+        if (users.contains(u1) and users.contains(u2))
+          {
+              users["users"][u1]["following"].remove(u2);
+              users["users"][u2]["followers"].remove(u1);
+              std::ofstream op_users_file("users.json");
+              op_users_file << std::setw(4) << users << std::endl;
+              response.set_success_status(0);
+          }
+          else:
+          {
+              response.set_success_status(1);
+          }
+          
+        return Status::OK;
+    }
+    
+    //if the command was "LIST"
+    Status getFollowersUsers(ServerContext* context, const list_request* request,
+    list_response* response)
+    {
+        //reading the json file
+        std::ifstream ip_users_file("users.json", std::ifstream::binary);
+        if (!ip_users_file.is_open())
+        {
+          std::cout << "Failed to open users.json " << std::endl;
+          return ;
+        }
+        
+        ip_users_file >> users;
+        std::vector<std::string> u1 = request.user1().name();
+        //u1 = request.user1();
+        
+        if (users.contains(u1))
+          {
+              std::vector<std::string> current_followers = users[u1]["followers"];
+              //for (Json::Value::ArrayIndex i = 0; i != users.size(); i++)
+              //{
+              Json::Value  v = users["users"];
+                  //number _of_users = v.size();
+              //the keys (names) in users json file is extracted
+              std::vector<std::string> active_users = v.getMemberNames();
+              
+              //setting the response variable
+              response.set_success_status(0);
+              for (vector<string>::iterator t=current_followers.begin(); t!=current_followers.end(); ++t)
+              {
+                  response.mutable_followers().add_followers(*t);
+              }
+              for (vector<string>::iterator t=active_users.begin(); t!=active_users.end(); ++t)
+              {
+                  response.mutable_active_users().add_active_users(*t);
+              }
+                  //for (auto const& key : v.getMemberNames())
+                  //{
+                      //active_users.push_back(key);
+                  //}
+                      
+              //}
+              
+          }
+          else:
+          {
+              response.set_success_status(1);
+          }
+          
+        return Status::OK;
+    }
+    
+ }
+
+
+
+
+
+
+
+
+
+void RunServer() {
+  std::string server_address("0.0.0.0:50051");
+  GreeterServiceImpl service;
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+
+
+
+
+int main(int argc, char** argv) {
+  RunServer();
+  return 0;
+}
