134c1327f645654151922692e8e0ef6370b188a2
shruthi <shruthi@client-10-229-136-29.tamulink.tamu.edu>
2020-02-04 12:30:58 -0600

    make error modified

--- tsc.cc
+++ tsc.cc
@@ -139,7 +139,7 @@ IReply Client::processCommand(std::string& input)
     
     IReply ire;
     //command = newString1[0];
-    std::string command = newString1[0];
+    char* command = newString1[0];
     
     if (strcmp(command, "FOLLOW")==0)
     {
