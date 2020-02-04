d5bd88d8b701545a5292c616562200a6db9247f2
shruthi <shruthi@client-10-229-136-29.tamulink.tamu.edu>
2020-02-04 16:30:16 -0600

    make error modified

--- tsd.cc
+++ tsd.cc
@@ -165,25 +165,24 @@ public:
         return Status::OK;
     }
     
- }
-
-
-
-
-
+};
 
 
 
 
 void RunServer() {
   std::string server_address("0.0.0.0:50051");
-  GreeterServiceImpl service;
+    
+  social_network service;
   ServerBuilder builder;
+    
   // Listen on the given address without any authentication mechanism.
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    
   // Register "service" as the instance through which we'll communicate with
   // clients. In this case it corresponds to an *synchronous* service.
   builder.RegisterService(&service);
+    
   // Finally assemble the server.
   std::unique_ptr<Server> server(builder.BuildAndStart());
   std::cout << "Server listening on " << server_address << std::endl;
