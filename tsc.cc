d16bd9d7faeddb7f490fe508395a1d764348de23
shruthi <shruthi@client-10-229-136-29.tamulink.tamu.edu>
2020-02-06 17:06:32 -0600

    follow error

--- tsc.cc
+++ tsc.cc
@@ -250,6 +250,7 @@ IReply Client::processCommand(std::string& input)
         
         Status status = stub_->getFollowersUsers(&context, l_request, &l_response);
         ire.grpc_status = status;
+        std::cout << "The status of the function is " << ire.grpc_status << std::endl;
         if (!status.ok() or l_response.success_status()!=0)
         {
             ire.comm_status = FAILURE_UNKNOWN;
