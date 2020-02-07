5a8c896746e29bb67bf321c95cd425685edd2b4e
shruthi <shruthi@client-10-229-136-29.tamulink.tamu.edu>
2020-02-07 17:34:43 -0600

    timeline modification

--- tsd.cc
+++ tsd.cc
@@ -312,7 +312,7 @@ public:
                     Json::Value updated_timeline = Json::arrayValue;
                     
                     //updating the timeline json object of the followers
-                    updated_timeline[0] = post->content();
+                    updated_timeline[0] = post.content();
                     int c = 1;
                     for(int i = 0; i<timeline_parsed[value].size()-1; i++)
                     {
