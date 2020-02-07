cbe9eae78c00c4faaa99f9ff79f9bbbd1dfa59a0
shruthi <shruthi@client-10-229-136-29.tamulink.tamu.edu>
2020-02-07 16:58:31 -0600

    timeline modification

--- tsc.cc
+++ tsc.cc
@@ -267,6 +267,15 @@ IReply Client::processCommand(std::string& input)
         
     }
     
+    else if (strcmp(command, "TIMELINE")==0)
+    {
+        //IReply ire;
+        //ire.comm_status = SUCCESS;
+        //ire.grpc_status = Status Status::OK;
+        processTimeline();
+        
+    }
+    
     //else if (strcmp(command, "TIMELINE")==0)
     //{
         //IReply ire;
